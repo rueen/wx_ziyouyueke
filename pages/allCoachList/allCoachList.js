68116b1412b7a9c887f8e56185b96d3923dc3176
diaochan <diaochan@seatent.com>
2025-10-29 19:51:28 +0800

    教练大厅

--- pages/allCoachList/allCoachList.js
+++ pages/allCoachList/allCoachList.js
@@ -12,6 +12,7 @@ Page({
     hasMore: true, // 是否还有更多数据
     currentPage: 1, // 当前页码
     pageSize: 10, // 每页数量
+    keyword: '', // 搜索关键词
   },
 
   /**
@@ -65,11 +66,17 @@ Page({
         return;
       }
 
-      const { currentPage, pageSize } = this.data;
+      const { currentPage, pageSize, keyword } = this.data;
       const params = {
         page: currentPage,
         limit: pageSize
+      };
+      
+      // 如果有搜索关键词，添加到参数中
+      if (keyword && keyword.trim()) {
+        params.keyword = keyword.trim();
       }
+      
       const res = await api.coaches.getList(params)
       if(res.success && res.data && res.data.list){
         const list = res.data.list.map(item => ({
@@ -77,6 +84,7 @@ Page({
         }))
         this.setData({
           list: this.data.currentPage === 1 ? list : [...this.data.list, ...list],
+          currentPage: currentPage + 1,
           isLoading: false,
           hasMore: res.data.page < res.data.totalPages
         });
@@ -93,6 +101,52 @@ Page({
         icon: 'none'
       });
     }
+  },
+
+  /**
+   * 搜索输入事件
+   * @param {Object} e 事件对象
+   */
+  onSearchInput(e) {
+    const keyword = e.detail.value;
+    this.setData({
+      keyword: keyword
+    });
+  },
+
+  /**
+   * 搜索确认事件（点击键盘搜索按钮）
+   * @param {Object} e 事件对象
+   */
+  onSearchConfirm(e) {
+    const keyword = e.detail.value || this.data.keyword;
+    this.setData({
+      keyword: keyword
+    });
+    this.loadList(true);
+  },
+
+  /**
+   * 清空搜索
+   */
+  onClearSearch() {
+    this.setData({
+      keyword: ''
+    });
+    this.loadList(true);
+  },
+
+  /**
+   * 点击教练卡片
+   * @param {Object} e 事件对象
+   */
+  onCoachTap(e) {
+    const { coach } = e.currentTarget.dataset;
+    if (!coach || !coach.id) return;
+    
+    wx.navigateTo({
+      url: `/pages/bindCoach/bindCoach?coachId=${coach.id}`
+    });
   }
 
 })
